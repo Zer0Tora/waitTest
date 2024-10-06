1c23e751d1ae03a02263a1a313e9b0605815b1b7
Zer0Tora <101511043+Zer0Tora@users.noreply.github.com>
2024-10-06 19:01:06 +0300

    Добавлен тест проверки появления кнопки через 5 сек

---
+++ src/test/java/org/example/waittest/MainPageTest.java
@@ -0,0 +1,44 @@
+package org.example.waittest;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class MainPageTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
+        options.addArguments("--remote-allow-origins=*");
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("https://demoqa.com/dynamic-properties");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void hideButtonTest() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#visibleAfter")));
+
+        assertTrue(driver.findElement(By.cssSelector("#visibleAfter")).isDisplayed(), "Кнопка не отображается");
+    }
+}
